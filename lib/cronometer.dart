6d229762b1cef2d0932975f8de4d929a76f0bbcd
Nerovelic <zack.vadhir2@hotmail.com>
2023-03-01 00:13:00 -0800

    funcionalidad del boton vueltas "flecha amarilla", como tambien añadido el container en forma de lista de las vueltas del cronometro

--- lib/cronometer.dart
+++ lib/cronometer.dart
@@ -14,6 +14,7 @@ class _CronometerState extends State<Cronometer> {
   int milisegundos = 0;
   bool estaCorriendo = false;
   late Timer timer;
+  List laps = [];
   void iniciarCronometro(){
     if (!estaCorriendo) {
       timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
@@ -35,12 +36,15 @@ class _CronometerState extends State<Cronometer> {
 
   void reiniciarCronometro(){   
       // ignore: unnecessary_this
-      this.milisegundos = 0; 
+      this.milisegundos = 0;
       setState(() {});
   }
 
   void vueltaCronometro(){
-    
+    String lap = formatearTiempo();
+    setState(() {
+      laps.add(lap);
+    });
   }
 
   String formatearTiempo(){
@@ -80,7 +84,33 @@ class _CronometerState extends State<Cronometer> {
             CupertinoButton(onPressed: detenerCronometro, child: const Icon(Icons.stop_circle_outlined, size: 50, color: Colors.red,))
           ],
         ),
-        CupertinoButton(onPressed: reiniciarCronometro, child: const Text("Restablecer"),)
+        CupertinoButton(onPressed: reiniciarCronometro, child: const Text("Restablecer"),),
+        const SizedBox(
+          height: 20.0,
+        ),
+        Container(
+          height: 300,
+          width: 400,
+          decoration: BoxDecoration(
+            color: const Color(0xFF323F68),
+            borderRadius: BorderRadius.circular(8.0)
+          ),
+          child: ListView.builder(
+            itemCount: laps.length,
+            itemBuilder: (context,index){
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Lap Nº ${index + 1}",style: const TextStyle(color: Colors.white, fontSize: 16.0),),
+                    Text("${laps[index]}",style: const TextStyle(color: Colors.white, fontSize: 16.0),)
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
       ],
     );
   }
